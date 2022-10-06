25a75bf313220bedb29ed20c81b6942893699fae
Rylen Enger <65436378+rylenenger@users.noreply.github.com>
2022-10-05 19:35:08 -0700

    esp32 connected through i2c with notecard

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <Notecard.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define usbSerial Serial
+#define productUID "com.gmail.rylenenger:iot_hub"
+
+Notecard notecard;
+
+void setup()
+{
+  delay(2500);
+  usbSerial.begin(115200);
+  notecard.begin();
+  notecard.setDebugOutputStream(usbSerial);
+  J *req = notecard.newRequest("hub.set");
+  JAddStringToObject(req, "product", productUID);
+  JAddStringToObject(req, "mode", "continuous");
+  notecard.sendRequest(req);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
